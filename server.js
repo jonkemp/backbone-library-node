1ceafc2385be345fd02651f1dbc5c92eede31b04
Jonathan Kemp <kempdogg@gmail.com>
2013-03-02 00:52:01 -0500

    Update server.js.

--- server.js
+++ server.js
@@ -5,112 +5,133 @@ var application_root = __dirname,
   mongoose = require('mongoose'); //MongoDB integration
 
 //Create server
-var app = express.createServer();
+var app = express();
 
 // Configure server
-app.configure(function () {
-  app.use(express.bodyParser()); //parses request body and populates req.body
-  app.use(express.methodOverride()); //checks req.body for HTTP method overrides
-  app.use(app.router); //perform route lookup based on url and HTTP method
-  app.use(express.static(path.join(application_root, "public"))); //Where to serve static content
-  app.use(express.errorHandler({ dumpExceptions:true, showStack:true })); //Show all errors in development
+app.configure( function() {
+    //parses request body and populates request.body
+    app.use( express.bodyParser() );
+
+    //checks request.body for HTTP method overrides
+    app.use( express.methodOverride() );
+
+    //perform route lookup based on url and HTTP method
+    app.use( app.router );
+
+    //Where to serve static content
+    app.use( express.static( path.join( application_root, 'site') ) );
+
+    //Show all errors in development
+    app.use( express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 //Start server
-app.listen(4711, function () {
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+var port = 4711;
+app.listen( port, function() {
+    console.log( 'Express server listening on port %d in %s mode', port, app.settings.env );
 });
 
 // Routes
-app.get('/api', function(req, res){
-  res.send('Library API is running');
+app.get( '/api', function( request, response ) {
+    response.send( 'Library API is running' );
 });
 
 //Connect to database
-mongoose.connect('mongodb://localhost/library_database');
+mongoose.connect( 'mongodb://localhost/library_database' );
 
 //Schemas
 var Book = new mongoose.Schema({
-  title:String,
-  author:String,
-  releaseDate:Date,
-  keywords:[Keywords]
-});
-
-var Keywords = new mongoose.Schema({
-  keyword:String
+    title: String,
+    author: String,
+    releaseDate: Date
 });
 
 //Models
-var BookModel = mongoose.model('Book', Book);
+var BookModel = mongoose.model( 'Book', Book );
 
 //Get a list of all books
-app.get('/api/books', function (req, res) {
-  return BookModel.find(function (err, books) {
-    if (!err) {
-      return res.send(books);
-    } else {
-      return console.log(err);
-    }
-  });
+app.get( '/api/books', function( request, response ) {
+    return BookModel.find( function( err, books ) {
+        if( !err ) {
+            return response.send( books );
+        } else {
+            return console.log( err );
+        }
+    });
 });
 
 //Insert a new book
-app.post('/api/books', function (req, res) {
-  var book = new BookModel({
-    title:req.body.title,
-    author:req.body.author,
-    releaseDate:req.body.releaseDate,
-    keywords:req.body.keywords
-  });
-  book.save(function (err) {
-    if (!err) {
-      return console.log('created');
-    } else {
-      return console.log(err);
-    }
-  });
-  return res.send(book);
-});
-
-app.get('/api/books/:id', function(req, res){
-  return BookModel.findById(req.params.id, function(err, book){
-    if(!err){
-      return res.send(book);
-    } else {
-      return console.log(err);
-    }
-  });
-});
-
-app.put('/api/books/:id', function(req, res){
-  console.log('Updating book ' + req.body.title);
-  return BookModel.findById(req.params.id, function(err, book){
-    book.title = req.body.title;
-    book.author = req.body.author;
-    book.releaseDate = req.body.releaseDate;
-    book.keywords = req.body.keywords;
-    return book.save(function(err){
-      if(!err){
-        console.log('book updated');
-      } else {
-        console.log(err);
-      }
-      return res.send(book);
+app.post( '/api/books', function( request, response ) {
+    var book = new BookModel({
+        title: request.body.title,
+        author: request.body.author,
+        releaseDate: request.body.releaseDate,
+        keywords: request.body.keywords
     });
-  });
+    book.save( function( err ) {
+        if( !err ) {
+            return console.log( 'created' );
+        } else {
+            return console.log( err );
+        }
+    });
+    return response.send( book );
 });
 
-app.delete('/api/books/:id', function(req, res){
-  console.log('Deleting book with id: ' + req.params.id);
-  return BookModel.findById(req.params.id, function(err, book){
-    return book.remove(function(err){
-      if(!err){
-        console.log('Book removed');
-        return res.send('');
-      } else {
-        console.log(err);
-      }
+//Update a book
+app.put( '/api/books/:id', function( request, response ) {
+    console.log( 'Updating book ' + request.body.title );
+    return BookModel.findById( request.params.id, function( err, book ) {
+        book.title = request.body.title;
+        book.author = request.body.author;
+        book.releaseDate = request.body.releaseDate;
+        book.keywords = request.body.keywords;
+
+        return book.save( function( err ) {
+            if( !err ) {
+                console.log( 'book updated' );
+            } else {
+                console.log( err );
+            }
+            return response.send( book );
+        });
     });
-  });
+});
+
+//Get a single book by id
+app.get( '/api/books/:id', function( request, response ) {
+    return BookModel.findById( request.params.id, function( err, book ) {
+        if( !err ) {
+            return response.send( book );
+        } else {
+            return console.log( err );
+        }
+    });
+});
+
+//Delete a book
+app.delete( '/api/books/:id', function( request, response ) {
+    console.log( 'Deleting book with id: ' + request.params.id );
+    return BookModel.findById( request.params.id, function( err, book ) {
+        return book.remove( function( err ) {
+            if( !err ) {
+                console.log( 'Book removed' );
+                return response.send( '' );
+            } else {
+                console.log( err );
+            }
+        });
+    });
+});
+
+//Schemas
+var Keywords = new mongoose.Schema({
+    keyword: String
+});
+
+var Book = new mongoose.Schema({
+    title: String,
+    author: String,
+    releaseDate: Date,
+    keywords: [ Keywords ]
 });
